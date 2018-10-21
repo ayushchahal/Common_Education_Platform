afaf66b8898eb8054ffa2bf6bf431f9fad03786b
ayushchahal <ayushchahal@gmail.com>
2018-10-21 23:39:10 +0530

    Thinking to implement labels for questions.

--- commonEducationPlatform/ROOT/javascript/changeSize.js
+++
@@ -1,6 +0,0 @@
-function changeSize()
-{
-	document.getElementsByClassName(" nicEdit-panelContain")[0].parentNode.setAttribute("style","width:100%");
-	document.getElementsByClassName(" nicEdit-main     ")[0].parentNode.setAttribute("style","width: 100%; border-width: 0px 1px 1px; border-top-style: initial; border-right-style: solid; border-bottom-style: solid; border-left-style: solid; border-top-color: initial; border-right-color: rgb(204, 204, 204); border-bottom-color: rgb(204, 204, 204); border-left-color: rgb(204, 204, 204); border-image: initial; overflow: hidden auto;");
-	document.getElementsByClassName(" nicEdit-main     ")[0].setAttribute("style","width: 98%; margin: 4px; min-height: 130px; overflow: hidden;");
-}
--- commonEducationPlatform/ROOT/javascript/clearAnswerFields.js
+++
@@ -1,6 +0,0 @@
-function clearAnswerFields()
-{
-	var ele = document.getElementsByName("options");
-	for(var i=0;i<ele.length;i++)
-	      ele[i].checked = false;
-}
